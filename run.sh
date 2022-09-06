95f38a5ae5e59b1256cf12a7bcc71d23dfd66af2
Ennio Visconti <ennio.visconti@gmail.com>
2022-09-06 18:12:16 +0200

    added runner file for the experiments

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+DATE=$(date +%F_%H-%M-%S) 
+
+CMD=`dune exec decent -- -n 5000 -nb_samples 1000 -msf 50 -st 1000 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
+
+eval "$CMD"
