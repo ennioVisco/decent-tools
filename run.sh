df504b917f839ba52e791ee4c8280eae923c69ce
Ennio Visconti <ennio.visconti@gmail.com>
2022-09-13 12:16:19 +0000

    updated running script

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 DATE=$(date +%F_%H-%M-%S) 
 
-CMD=`dune exec decent -- -n 5000 -nb_samples 1000 -msf 50 -st 1000 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
+#CMD=`dune exec decent -- -n 5000 -nb_samples 1000 -msf 50 -st 1000 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
+
+CMD=`dune exec decent -- -n 50000 -nb_samples 1000 -msf 50 -st 1000 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
 
 eval "$CMD"
