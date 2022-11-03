da870ea1365498f7d0a66d6358ab4becca345c40
Ennio Visconti <ennio.visconti@gmail.com>
2022-11-03 09:10:22 +0000

    basic run script

--- run.sh
+++ run.sh
@@ -2,8 +2,13 @@
 
 DATE=$(date +%F_%H-%M-%S) 
 
-#CMD=`dune exec decent -- -n 5000 -nb_samples 1000 -msf 50 -st 1000 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
+# time = 1000
+#CMD=`dune exec decent -- -n 50000 -nb_samples 1000 -msf 50 -st 1000 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
 
-CMD=`dune exec decent -- -n 50000 -nb_samples 1000 -msf 50 -st 1000 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
+# time = 100
+CMD=`dune exec decent -- -n 500000 -nb_samples 1000 -msf 50 -st 100 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
+
+# time = 100; formula length = 15
+#CMD=`dune exec decent -- -n 500000 -nb_samples 1000 -msf 50 -st 100 -dalpha "{a|b|c}" -prt_full true -keep_samples true -seed 3 -file $DATE\_output.log -abs true -exis true -bexis true -univ true -prec true -resp true -precc true -respc true -consc true`
 
 eval "$CMD"
